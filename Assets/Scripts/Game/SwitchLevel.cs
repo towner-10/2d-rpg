12343712338b73874bf1b9aa7e93a3d6c0224b0e
Collin Town <collintown@gmail.com>
2018-11-03 11:17:09 -0400

    Added Level Change

---
+++ Assets/Scripts/Game/SwitchLevel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchLevel : MonoBehaviour {
+
+    public Object scene;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            SceneManager.LoadScene(scene.name, LoadSceneMode.Single);
+        }
+    }
+}
